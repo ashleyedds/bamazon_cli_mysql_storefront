00d30d48fe3162b70c67c23d7348541cf114416e
Ashley Edds <ashley@edds.net>
2018-03-02 15:02:55 -0500

    working customer interface

---
+++ bamazonCustomer.js
@@ -0,0 +1,124 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require("cli-table");
+
+var connection = mysql.createConnection(
+    {
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "pepper12",
+        database: "bamazon"
+    }
+);
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("\nHello! Welcome to our store!\n");
+
+    initStore();
+});
+
+function initStore() {
+    connection.query("select * from products", function (err, results) {
+
+        var table = new Table({
+            chars: { 
+                "top": "═", "top-mid": "╤", "top-left": "╔", "top-right": "╗",
+                "bottom": "═", "bottom-mid": "╧", "bottom-left": "╚", "bottom-right": "╝",
+                "left": "║", "left-mid": "╟", "mid": "─", "mid-mid": "┼",
+                "right": "║", "right-mid": "╢", "middle": "│"
+            }
+        });
+
+        table.push (
+            ["ID", "Product", "Department", "Price", "Stock"]);
+
+        for (var i = 0; i < results.length; i++){
+            table.push (
+                [results[i].item_id, results[i].product_name, results[i].department_name, results[i].price, results[i].stock_quantity]
+            );
+        }
+
+    
+        console.log(table.toString());
+
+        inquirer.prompt([
+            {
+                name: "itemQuestion",
+                type: "list",
+                message: "\nWhich item would you like to purchase?",
+                choices: function () {
+                    var itemArray = [];
+                    for (var i = 0; i < results.length; i++) {
+                        itemArray.push(results[i].product_name);
+                    }
+                    return itemArray;
+                }
+            },
+            {
+                name: "itemQuantity",
+                type: "input",
+                message: "How many would you like to buy?"
+            }
+        ]).then(function (response) {
+            //grab name of item chosen to purchase
+            var chosenItem;
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].product_name === response.itemQuestion) {
+                    chosenItem = results[i];
+                }
+            }
+
+            //grab quantity requested to purchase
+            var itemAmount = response.itemQuantity;
+
+            //check to see if there is enough stock
+            if (chosenItem.stock_quantity < itemAmount) {
+                console.log("I'm sorry, we don't have enough of that item in stock to process your request. Please try again.\n")
+                return initStore();
+            }
+
+            //confirm purchase
+            inquirer.prompt([
+                {
+                    name: "confirm",
+                    type: "confirm",
+                    message: "You have selected to purchase " + itemAmount + " of the " + chosenItem.product_name + ". Are you sure you'd like to buy this?"
+                }
+            ]).then(function(answer){
+                if (answer.confirm === false){
+                    console.log("No worries. Transaction ended. Please come back again!")
+                    return connection.end();
+                }
+                //finish transaction
+
+            //calculate total trasaction cost
+            var totalCost = itemAmount * chosenItem.price;
+            console.log("Your total cost is $" + totalCost);
+
+            //subtract amount purchased
+            var newStock = chosenItem.stock_quantity - itemAmount;
+
+            //update the database with the new stock quantity
+            connection.query("UPDATE products SET ? WHERE ?",
+                [{
+                    stock_quantity: newStock
+                },
+                {
+                    product_name: chosenItem.product_name
+                }],
+                function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log("Thank you for your purchase! We appreciate your business. Please come again!");
+                    connection.end();
+                }
+            )
+            })
+
+        
+        })
+    });
+};
